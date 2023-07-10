d1541c3cba97ddf3e3578f90788a9325d2c861bd
cnetsec <86935257+cnetsec@users.noreply.github.com>
2023-07-10 14:51:53 -0300

    Update fortifysynctime.py

--- fortifysynctime/fortifysynctime.py
+++ fortifysynctime/fortifysynctime.py
@@ -1,14 +1,14 @@
 import requests
 import re
 
-base_url = 'https://app.conviso.com.br/scopes/COMPANYID/integrations/fortify'
+base_url = 'https://app.conviso.com.br/scopes/REPLACECOMPANYID/assets/REPLACEASSETID?locale=en'
 headers = {
     'authority': 'app.conviso.com.br',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6',
-    'X-Armature-Api-Key': '',
+    'X-Armature-Api-Key': 'REPLACEAPIKEY',
     'if-none-match': 'W/"eb6d93c4f9c8a3febb0bff41f3525ab2"',
-    'referer': 'https://app.conviso.com.br/scopes/COMPANYID/integrations/fortify?utf8=%E2%9C%93&status=error&name=&button=',
+    'referer': 'https://app.conviso.com.br/scopes/REPLACECOMPANYID/integrations/fortify?utf8=%E2%9C%93&status=error&name=&button=',
     'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -20,29 +20,23 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
 }
 
-for page in range(1, 18):
-    params = {
-        'button': '',
-        'locale': 'en',
-        'name': '',
-        'page': page,
-        'status': 'success',
-        'utf8': '✓'
-    }
+params = {
+    'button': '',
+    'locale': 'en',
+    'name': '',
+    'page': 1,
+    'status': 'success',
+    'utf8': '✓'
+}
 
-    response = requests.get(base_url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.text
+response = requests.get(base_url, headers=headers, params=params)
+if response.status_code == 200:
+    data = response.text
 
-        # Realize a busca pelos padrões usando expressões regulares
-        name_pattern = r'ASSETNAME'
-        date_pattern = r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2} [AP]M'
+    date_pattern = r'\b([0-9]{2}/){2}[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2} [AP]M\b'
 
-        name_matches = re.findall(name_pattern, data)
-        date_matches = re.findall(date_pattern, data)
+    date_match = re.search(date_pattern, data)
 
-        if name_matches and date_matches:
-            name = name_matches[0]
-            date = date_matches[0]
-            print(f'{name} - {date}')
-            break  # Encerra a execução após encontrar o resultado
+    if date_match:
+        date = date_match.group()
+        print(f'{date}')
