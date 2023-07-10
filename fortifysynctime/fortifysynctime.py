d9c9ec68689bac381a516d7eb08d0535c67ca019
cnetsec <86935257+cnetsec@users.noreply.github.com>
2023-07-10 14:38:21 -0300

    Update fortifysynctime.py

--- fortifysynctime/fortifysynctime.py
+++ fortifysynctime/fortifysynctime.py
@@ -6,7 +6,7 @@ headers = {
     'authority': 'app.conviso.com.br',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6',
-    'X-Armature-Api-Key': 'APIKEY',
+    'X-Armature-Api-Key': '',
     'if-none-match': 'W/"eb6d93c4f9c8a3febb0bff41f3525ab2"',
     'referer': 'https://app.conviso.com.br/scopes/COMPANYID/integrations/fortify?utf8=%E2%9C%93&status=error&name=&button=',
     'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
@@ -35,14 +35,14 @@ for page in range(1, 18):
         data = response.text
 
         # Realize a busca pelos padrões usando expressões regulares
-        name_pattern = r'ASSET'
-        date_pattern = r'\b([0-9]{2}/){2}[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2} [AP]M\b'
+        name_pattern = r'ASSETNAME'
+        date_pattern = r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2} [AP]M'
 
-        name_match = re.search(name_pattern, data)
-        date_match = re.search(date_pattern, data)
+        name_matches = re.findall(name_pattern, data)
+        date_matches = re.findall(date_pattern, data)
 
-        if name_match and date_match:
-            name = name_match.group()
-            date = date_match.group()
+        if name_matches and date_matches:
+            name = name_matches[0]
+            date = date_matches[0]
             print(f'{name} - {date}')
             break  # Encerra a execução após encontrar o resultado
